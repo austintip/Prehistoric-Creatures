e40845306c34d3c69ae6e5160ff688e1cf1ba19d
austintip <austin.d.tipograph@gmail.com>
2021-01-11 20:02:42 -0500

    made stuff work

---
+++ routes/dinos.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+//index
+router.get('/', (req, res) => {
+    //read the file that stores all my dinos and save in a variable to use later
+    let dinos = fs.readFileSync('./dinos.json');
+    let dinoData = JSON.parse(dinos)
+    console.log(dinoData);
+    res.render('dinos/index', { dinos: dinoData })
+});
+
+// New route - /dinos/new
+router.get('/new', (req, res) => {
+    console.log('--------- New dino who dis?');
+    res.render('dinos/new');
+});
+
+
+// Create - POST /dinos
+router.post('/', (req, res) => {
+    console.log(req.body);
+    // turn dinos.json into a mutable array
+    let dinos = fs.readFileSync('./dinos.json')
+    dinosJS = JSON.parse(dinos);
+
+    //add new dino from req.body to the array
+    dinosJS.push(req.body);
+
+    // turn dino array into json
+    let dinoJSON = JSON.stringify(dinosJS);
+
+    // write to dinos.json
+    fs.writeFileSync('./dinos.json', dinoJSON);
+
+    res.redirect('/dinos');
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const layouts = require('express-ejs-layouts');
 const path = require('path');
 const app = express();
+app.use(express.urlencoded({ extended: false })); //Body parsing middleware
 
 app.set('view engine', 'ejs');
 app.use(layouts);
@@ -10,5 +11,7 @@ app.use(express.static(path.join(__dirname, '/static')));
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+app.use('/dinos', require('./routes/dinos'));
 
 app.listen(8000, () => console.log('Hey, listen!'));
